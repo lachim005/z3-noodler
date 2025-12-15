45c64f52afc004501a7ddf1e2ee063d84dc13720
jurajsic <juraj.sic@outlook.com>
2025-12-15 10:12:05 +0100

    fix always_check in check_len_sat

--- src/smt/theory_str_noodler/theory_str_noodler_final_check.cpp
+++ src/smt/theory_str_noodler/theory_str_noodler_final_check.cpp
@@ -746,7 +746,7 @@ namespace smt::noodler {
     }
 
     lbool theory_str_noodler::check_len_sat(expr_ref len_formula, bool check_with_context, expr_ref* unsat_core, bool always_check) {
-        if (!always_check || (len_formula == m.mk_true() && len_vars.empty())) {
+        if (!always_check && len_formula == m.mk_true() && len_vars.empty()) {
             // we assume here that existing length constraints are satisfiable, so adding true will do nothing
             // however, for model generation, we need to always produce models if we have some length vars
             return l_true;
